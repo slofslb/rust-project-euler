ab003b5b7176db806903331077e61b01154fcf9c
shen longbin <slofslb@qq.com>
2019-09-04 20:49:41 +0800

    config debug

---
+++ euler1/target/debug/deps/euler1-8569b5aa133ef053.d
@@ -0,0 +1,5 @@
+C:\Users\ccc\rust-project-euler\euler1\target\debug\deps\euler1-8569b5aa133ef053.exe: src\main.rs
+
+C:\Users\ccc\rust-project-euler\euler1\target\debug\deps\euler1-8569b5aa133ef053.d: src\main.rs
+
+src\main.rs:
---
+++ euler1/target/debug/euler1.d
@@ -0,0 +1,1 @@
+C:\Users\ccc\rust-project-euler\euler1\target\debug\euler1.pdb: C:\Users\ccc\rust-project-euler\euler1\src\main.rs
