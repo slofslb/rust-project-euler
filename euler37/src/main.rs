b681bdb47d325569a273d3665d293e2cc65c81a9
shen longbin <slofslb@qq.com>
2019-09-05 22:07:56 +0800

    clear logic, add functions

--- euler37/src/main.rs
+++ euler37/src/main.rs
@@ -1,50 +1,54 @@
 fn main() {
     let max_number_to_check = 1_000_000;
-
     let mut prime_mask = vec![true; max_number_to_check];
+    fill_prime_mask(&mut prime_mask);
+
+    let mut sum = 0;
+    for n in 10..max_number_to_check {
+        if is_trun_prime_right(&prime_mask, n) && is_trun_prime_left(&prime_mask, n) {
+            println!("{}", n);
+            sum += n;
+        }
+    }
+    println!("sum: {}", sum);
+}
+
+fn is_trun_prime_right(prime_mask: &Vec<bool>, n: usize) -> bool {
+    let mut s = n.to_string();
+    while s.len() > 0 {
+        let p = s.parse::<usize>().unwrap();
+        if !prime_mask[p] {
+            return false;
+        }
+        s.remove(0);
+    }
+    true
+}
+
+fn is_trun_prime_left(prime_mask: &Vec<bool>, n: usize) -> bool {
+    let mut s = n.to_string();
+    while s.len() > 0 {
+        let p = s.parse::<usize>().unwrap();
+        if !prime_mask[p] {
+            return false;
+        }
+        s.pop();
+    }
+    true
+}
+
+fn fill_prime_mask(prime_mask: &mut Vec<bool>) {
     prime_mask[0] = false;
     prime_mask[1] = false;
 
     const FIRST_PRIME_NUMBER: usize = 2;
-    for p in FIRST_PRIME_NUMBER..max_number_to_check {
+    for p in FIRST_PRIME_NUMBER..prime_mask.len() {
         if prime_mask[p] {
             let mut i = 2 * p;
-            while i < max_number_to_check {
+            while i < prime_mask.len() {
                 prime_mask[i] = false;
                 i += p;
             }
         }
     }
-
-    let mut sum = 0;
-    for n in 10..max_number_to_check {
-        if !prime_mask[n] {continue;}
-        let mut s = n.to_string();
-
-        let mut is_trun_prime = true;
-        while { s.pop(); s.len() > 0} {
-            let p = s.parse::<usize>().unwrap();
-            if !prime_mask[p] {
-                is_trun_prime = false;
-                break;
-            }
-        }
-
-        if !is_trun_prime { continue; }
-
-        s = n.to_string();
-        while s.len() > 1 {
-            s.remove(0); 
-            let p = s.parse::<usize>().unwrap();
-            if !prime_mask[p] {
-                is_trun_prime = false;
-                break;
-            }
-        }
-        if !is_trun_prime { continue; }
-        println!("{}", n);
-        sum += n;
-        
-    }
-    println!("sum: {}", sum);
 }
