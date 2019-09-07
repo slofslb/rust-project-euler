759d5fb75c947bfe9f51bde763cb3bb605a25b21
shen longbin <slofslb@qq.com>
2019-09-08 06:43:30 +0800

    add .gitignore

--- euler15/target/debug/deps/euler15-774bcae2903d212c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ccc\rust-project-euler\euler15\target\debug\deps\euler15-774bcae2903d212c.exe: src\main.rs
-
-C:\Users\ccc\rust-project-euler\euler15\target\debug\deps\euler15-774bcae2903d212c.d: src\main.rs
-
-src\main.rs:
--- euler15/target/debug/euler15.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ccc\rust-project-euler\euler15\target\debug\euler15.pdb: C:\Users\ccc\rust-project-euler\euler15\src\main.rs
