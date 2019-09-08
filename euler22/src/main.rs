64670c67c44bcfa47f1dd43be44b844e8b23c236
shen longbin <slofslb@qq.com>
2019-09-08 20:09:34 +0800

    git ignore

--- euler22/src/main.rs
+++ euler22/src/main.rs
@@ -1,25 +1,21 @@
-
 fn main() {
+    let data = std::fs::read_to_string("names.txt").expect("读文件失败");
+    let data2 = remove_quote(&data);
+    let mut names: Vec<&str> = data2.split(",").collect();
 
+    names.sort();
 
-let data = std::fs::read_to_string("names.txt").expect("读文件失败");
-let data2 = remove_quote(&data);
-let mut names: Vec<&str> = data2.split(",").collect();
-
-names.sort();
-
-let mut score = 0;
-for (i, name) in names.iter().enumerate() {
-    let ws = word_score(name);
-    println!("{} {} {}", (i + 1), name, ws);
-    score += ws * (i + 1);
-}
-println!("{}", score);
-
+    let mut score = 0;
+    for (i, name) in names.iter().enumerate() {
+        let ws = word_score(name);
+        println!("{} {} {}", (i + 1), name, ws);
+        score += ws * (i + 1);
+    }
+    println!("{}", score);
 }
 
 fn remove_quote(s: &str) -> String {
-    s.chars().filter(|c| *c !='"').collect()
+    s.chars().filter(|c| *c != '"').collect()
 }
 
 fn letter_number(ch: char) -> usize {
@@ -34,5 +30,3 @@ fn word_score(word: &str) -> usize {
     }
     score
 }
-
-
--- euler22/target/debug/euler22.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ccc\rust-project-euler\euler22\target\debug\euler22.pdb: C:\Users\ccc\rust-project-euler\euler22\src\main.rs
