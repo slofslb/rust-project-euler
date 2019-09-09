e6c0e980784e3e463567702928e1e19f2ed563b2
shen longbin <slofslb@qq.com>
2019-09-09 18:35:46 +0800

    version 2, use diag array( only has 4 items)

--- euler58/src/main.rs
+++ euler58/src/main.rs
@@ -6,24 +6,16 @@ fn main() {
     let mut count = 0;
     for n in (3..).step_by(2) {
         let lower_right = n * n;
-        if pset.is_prime(lower_right) {
-            count += 1;
-        }
-        let lower_left = lower_right - n + 1;
-        if pset.is_prime(lower_left) {
-            count += 1;
-        }
-        let upper_left = lower_left - n + 1;
-        if pset.is_prime(upper_left) {
-            count += 1;
-        }
-        let upper_right = upper_left - n + 1;
-        if pset.is_prime(upper_right) {
-            count += 1;
+
+        let diag = [lower_right, lower_right - (n-1), lower_right - (n-1)*2, lower_right - (n-1)*3];
+        for d in diag.iter() {
+            if pset.is_prime(*d) {
+                count += 1;
+            }
         }
         let percent = (count as f32) / ((2 * n - 1) as f32);
         if percent < 0.1 {
-            println!("{} {} {}", n, count, percent);
+            println!("{} count: {} percent: {}", n, count, percent);
             break;
         }
     }
