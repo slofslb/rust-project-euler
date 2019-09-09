6bd616c620872c9c3e3fc107d6e9c15f0d403b29
shen longbin <slofslb@qq.com>
2019-09-09 18:43:56 +0800

    使用函数式编程，生成对角线上的4个元素，并统计素数的个数

--- euler58/src/main.rs
+++ euler58/src/main.rs
@@ -3,20 +3,19 @@ use primes::PrimeSet;
 fn main() {
     let mut pset = PrimeSet::new();
 
-    let mut count = 0;
+    let mut count_prime = 0;
     for n in (3..).step_by(2) {
-        let lower_right = n * n;
+        let lower_right = n * n; 
+        count_prime += (0..4) 
+            .map(|x| lower_right - (n - 1) * x) // 四个对象线上的数字
+            .filter(|&x| pset.is_prime(x))
+            .count();
 
-        let diag = (0..4).map(|x| lower_right - (n-1)*x);
-        for d in diag {
-            if pset.is_prime(d) {
-                count += 1;
-            }
-        }
-        let percent = (count as f32) / ((2 * n - 1) as f32);
+        let percent = (count_prime as f32) / ((2 * n - 1) as f32);
         if percent < 0.1 {
-            println!("{} count: {} percent: {}", n, count, percent);
+            println!("{} count: {} percent: {}", n, count_prime, percent);
             break;
         }
     }
 }
+// 26241 count: 5248 percent: 0.099998094
