7d9fb728ecf312c953181b76664b710bf1676bbd
Shen Longbin <slofslb@qq.com>
2019-09-09 16:50:40 +0800

    (1..100).rev()，显示内容少一些

--- euler56/src/main.rs
+++ euler56/src/main.rs
@@ -3,15 +3,18 @@ use num_bigint::BigUint;
 
 fn main() {
     let mut max_sum = 0;
-    for a in 1..100 {
-        for b in 1..100 {
+    for a in (1..100).rev() {
+        for b in (1..100).rev() {
             let s = power(a, b).to_string();
             let sum_digits = s.chars().map(|ch| ch.to_digit(10).unwrap()).sum::<u32>();
             if sum_digits > max_sum {
                 max_sum = sum_digits;
                 println!(
                     "a: {} b: {} len: {} sum of digits: {}",
-                    a, b, s.len(), sum_digits
+                    a,
+                    b,
+                    s.len(),
+                    sum_digits
                 );
             }
         }
