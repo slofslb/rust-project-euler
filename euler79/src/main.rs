5e8abeedd012216b5d9677a8da33bdad085703ea
shen longbin <slofslb@qq.com>
2019-09-11 09:10:20 +0800

    visiualizing by graphviz

--- euler79/src/main.rs
+++ euler79/src/main.rs
@@ -2,19 +2,32 @@ fn main() {
     let data = std::fs::read_to_string("keylog.txt").expect("打开文件失败");
     let lines = data.split("\n");
 
-    let mut kv : Vec<(&str, &str)> = vec![];
+    let mut kv: Vec<(&str, &str)> = vec![];
 
     for line in lines {
-        if line.len() < 3 {continue;}
+        if line.len() < 3 {
+            continue;
+        }
         let a = &line[..1];
-        let b = &line[1..2];
+        let b = &line[1..=1];
         let c = &line[2..];
-        if !kv.contains(&(a,b)) {
-            kv.push((a,b));
-        }
-        if !kv.contains(&(b,c)) {
-            kv.push((b,c));
-        }
+        kv.push((a, b));
+        kv.push((b, c));
+        /*        if !kv.contains(&(a,b)) {
+                    kv.push((a,b));
+                }
+                if !kv.contains(&(b,c)) {
+                    kv.push((b,c));
+                }
+        */
     }
-        println!("{:?}", kv );
+
+    // generate a graphviz format file
+    println!("digraph G {{");
+    println!("rankdir=LR;");
+    for (a, b) in kv {
+        println!("{} -> {};", a, b);
+    }
+    println!("}}");
 }
+// 73162890
