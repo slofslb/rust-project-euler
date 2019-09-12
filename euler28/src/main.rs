74c5aaaf6c0a7fb8ce9053e192052fd694fa3d35
shen longbin <slofslb@qq.com>
2019-09-12 15:14:12 +0800

    step_by()

--- euler28/src/main.rs
+++ euler28/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let mut sum = 1;
-    for i in 1..=500 {
-        let upperright = (i*2+1) * (i*2+1);
+    for i in (3..=1001).step_by(2) {
+        let upperright = i * i;
         sum += upperright;
-        sum += upperright - (i*2) * 1;
-        sum += upperright - (i*2) * 2;
-        sum += upperright - (i*2) * 3;
+        sum += upperright - (i - 1) * 1;
+        sum += upperright - (i - 1) * 2;
+        sum += upperright - (i - 1) * 3;
     }
     println!("{}", sum);
 }
