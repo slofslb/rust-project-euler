fd074b6122e16bb2cfefb61a80c6a8a3d04f1366
shen longbin <slofslb@qq.com>
2019-09-14 17:55:16 +0800

    增加一些不同的写法

--- euler34/src/main.rs
+++ euler34/src/main.rs
@@ -9,10 +9,15 @@ fn main() {
     let mut sum = 0;
     for n in 3..999999 {
         // 各位数字的阶乘之和
-        let mut sum_fac = 0;
-        for digit in n.to_string().chars().map(|x| x.to_digit(10).unwrap()) {
-            sum_fac += fac[digit as usize];
-        }
+        // let mut sum_fac = 0;
+        // for digit in n.to_string().chars().map(|x| x.to_digit(10).unwrap()) {
+        //     sum_fac += fac[digit as usize];
+        // }
+        let sum_fac: u32 = n
+            .to_string()
+            .chars()
+            .map(|x| fac[x.to_digit(10).unwrap() as usize])
+            .sum();
         if n == sum_fac {
             println!("{}! = {}", n, n);
             sum += n;
