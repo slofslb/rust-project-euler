3926e49651d3ffc3d618577371b18f2b46fd05f4
Shen Longbin <slofslb@qq.com>
2019-09-16 09:53:41 +0800

    first version

---
+++ euler71/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("3/7= {}", 3.0 / 7.0);
+    let mut max = 0.0;
+    for numerator in (2..=1_000_000).rev() {
+        let x = numerator * 3 / 7;
+        if x * 7 == numerator * 3 {
+            continue;
+        }
+        let q = (x as f64) / (numerator as f64);
+        if q > max {
+            max = q;
+            println!("{} / {} = {}", x, numerator, q);
+        }
+    }
+}
