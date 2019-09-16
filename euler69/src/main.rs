bc55b0f8b26131216de59495b12b41f2ae9f42d9
Shen Longbin <slofslb@qq.com>
2019-09-16 08:16:55 +0800

    first version

--- euler69/src/main.rs
+++ euler69/src/main.rs
@@ -1,40 +1,36 @@
-use hashbrown::HashMap;
+use primes::PrimeSet;
 
 fn main() {
-    let mut cache :HashMap<(u64,u64), u64> = HashMap::new();
-
-    let mut max_n_phi = 0_f32;
-    for n in 2..=10 {//1_000_000 {
-        if n % 10000 == 0{
-            println!("{}", n);
-        }
+    let mut max_ratio_n_phi = 0_f32;
+    for n in 2..=3000 {
         let mut phi = 0;
         for i in 1..n {
-            if primes::is_prime(n) || primes::is_prime(i)
-             || gcd(&mut cache, i, n) == 1 {
+            // 最大公约数为1，表示互质
+            if gcd(i, n) == 1 {
                 phi += 1;
             }
         }
-        let n_phi = (n as f32) / (phi as f32);
-        if n_phi > max_n_phi {
-            println!("n={} n/phi={}", n, n_phi);
-            max_n_phi = n_phi;
+        let ratio_n_phi = (n as f32) / (phi as f32);
+        if ratio_n_phi > max_ratio_n_phi {
+            println!("n= {}  n/phi= {}", n, ratio_n_phi);
+            max_ratio_n_phi = ratio_n_phi;
         }
+    }
+    let mut pset = PrimeSet::new();
+    let mut prod = 1;
+    for p in pset.iter() {
+        prod *= p;
+        if prod > 1_000_000 {
+            break;
+        }
+        println!("{}", prod);
     }
 }
 
-fn gcd(cache: &mut HashMap<(u64, u64), u64>, a: u64, b: u64) -> u64 {
+fn gcd(a: u64, b: u64) -> u64 {
     if b == 0 {
         a
     } else {
-        if cache.contains_key(&(a, b)) {
-            //println!("cache! {:?}", (a,b) );
-            return *cache.get(&(a,b)).unwrap();
-        }
-        let temp = gcd(cache, b, a % b);
-        if a < b && b < 100000 {
-            cache.insert((a,b), temp);
-        }
-        temp
+        gcd(b, a % b)
     }
 }
