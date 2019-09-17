27962acf2b85eac7da8cab1f24d50c9a1ed77cc6
shen longbin <slofslb@qq.com>
2019-09-17 19:11:16 +0800

    slowly

--- euler493/src/main.rs
+++ euler493/src/main.rs
@@ -12,12 +12,22 @@ fn main() {
             balls.push((color, i));
         }
     }
-   let sample = seq::sample_slice(&mut rng, &balls, 20);
-   let mut distinct_colors = vec![];
-   for (color, _) in &sample {
-       if !distinct_colors.contains(&color) {
-           distinct_colors.push(color)
-       }
-   }
-   println!("{:?} \n{:?}", sample, distinct_colors );
+
+    let mut count = 0;
+    let total_count = 1000000000;
+    for i in 1..=total_count {
+        let sample = seq::sample_slice(&mut rng, &balls, 20);
+        let mut distinct_colors = vec![];
+        for (color, _) in &sample {
+            if !distinct_colors.contains(&color) {
+                distinct_colors.push(color);
+                if distinct_colors.len() == 7 {break;}
+            }
+        }
+        //println!("{:?} \n{:?}", sample, distinct_colors.len() );
+        count += distinct_colors.len();
+        if i % 1000000 == 0 {
+            println!("{}", (count as f64) / (i as f64));
+        }
+    }
 }
