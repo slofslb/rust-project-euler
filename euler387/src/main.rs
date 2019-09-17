a5ebbe28a050ce944975c46a5c15d05c5fceda4d
Shen Longbin <slofslb@qq.com>
2019-09-17 15:09:55 +0800

    change a n algorithm

--- euler387/src/main.rs
+++ euler387/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let max_number_to_check = 100_000_000;
+    let max_number_to_check = 1_000_000;
     let mut prime_mask = vec![true; max_number_to_check];
     prime_mask[0] = false;
     prime_mask[1] = false;
@@ -15,6 +15,16 @@ fn main() {
     }
     println!("finished prime generation");
 
+//    for n in 10..100000 {
+//         if right_harshad(n) {
+//                     println!("{}", n);
+//         }
+//     }
+for i in 1..=9 {
+right_har(i);
+}
+
+/*
     let mut sum = 0;
     for n in 10..max_number_to_check/10 {
     //println!("{}", harshad(201));
@@ -32,12 +42,42 @@ fn main() {
         }
     }
     println!("sum: {}", sum);
+    */
 }
 
+fn right_har(start: u64) {
+    if start >= 999999999 {return;}
+    if right_harshad(start) {
+        if strong(start as usize) {
+            //println!("{}", start);
+            for j in (1..=9).step_by(2) {
+                let temp = start * 10 + j;
+                if primes::is_prime(temp) {
+                    println!("{}", temp);
+                    //sum += temp;
+                }
+            }
+        }
+        for i in 0..=9 {
+            right_har(start * 10 + i);
+        }
+    }
+}
 fn harshad(n: u64) -> bool {
     let sum_digits: u32 = n.to_string().chars().map(|c| c.to_digit(10).unwrap()).sum();
     n % (sum_digits as u64) == 0
 }
+
+/*
+fn is_prime(n:u64) -> bool {
+    for i in 2..(n as f64).sqrt() as u64 {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+*/
 
 fn right_harshad(n: u64) -> bool {
     let digits: Vec<u32> = n
@@ -45,11 +85,11 @@ fn right_harshad(n: u64) -> bool {
         .chars()
         .map(|c| c.to_digit(10).unwrap())
         .collect();
-    let mut num = 0;
-    let mut sum = 0;
+    let mut num:u64 = 0;
+    let mut sum:u64 = 0;
     for d in digits {
-        num = num * 10 + d;
-        sum += d;
+        num = num * 10 + d as u64;
+        sum += d as u64;
         if num % sum != 0 {
             return false;
         }
@@ -73,4 +113,22 @@ fn strong_right_harshad(prime_mask: &Vec<bool>, n: usize) -> bool {
         }
     }
     prime_mask[(n as usize) / (sum as usize)]
+}
+
+fn strong(n: usize) -> bool {
+    let digits: Vec<u32> = n
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+    let mut num = 0;
+    let mut sum = 0;
+    for d in digits {
+        num = num * 10 + d;
+        sum += d;
+        if num % sum != 0 {
+            return false;
+        }
+    }
+    primes::is_prime((n as u64) / (sum as u64))
 }
