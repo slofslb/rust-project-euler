37e78d2039d3e07ba2618ca90ccf6cd2680ff519
shen longbin <slofslb@qq.com>
2019-09-21 21:48:39 +0800

    使用format!

--- euler36/src/main.rs
+++ euler36/src/main.rs
@@ -2,17 +2,22 @@ fn main() {
     let mut sum = 0;
     for n in 1..1_000_000 {
         let s10 = n.to_string();
-        let s2 = to_str_radix2(n);
-        if s10 == s10.chars().rev().collect::<String>() 
-            && s2 == s2.chars().rev().collect::<String>() {
-            println!("{}", n);
+        let s2 = format!("{:b}", n);
+        //let s2 = to_radix2_string(n);
+        if is_palindromic(s10) && is_palindromic(s2) {
+            println!("{} {:b}", n, n);
             sum += n;
         }
     }
     println!("sum: {}", sum);
 }
 
-fn to_str_radix2(n: u32) -> String {
+fn is_palindromic(s:String) -> bool {
+
+    s == s.chars().rev().collect::<String>() 
+}
+
+fn to_radix2_string(n: u64) -> String {
     let mut d = n;
     let mut s:String = "".to_string();
     while d != 0 {
