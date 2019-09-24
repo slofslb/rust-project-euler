56b4997a778c069d3e28954efdd83e7aad6b6a7d
Shen Longbin <slofslb@qq.com>
2019-09-24 22:29:38 +0800

    精简函数

--- euler42/src/main.rs
+++ euler42/src/main.rs
@@ -5,20 +5,19 @@ fn main() {
     // 准备足够的三角数
     let mut tri_numbers = vec![];
     for i in 1..100 {
-        tri_numbers.push(i * (i+1) / 2);
+        tri_numbers.push(i * (i + 1) / 2);
     }
     println!("{:?}", tri_numbers);
 
-
     let data = std::fs::read_to_string("words.txt").expect("读文件失败");
-    let data2 = remove_quote(&data);
+    let data2: String = data.chars().filter(|c| *c != '"').collect();
     let names: Vec<&str> = data2.split(",").collect();
 
     let mut count = 0;
     for name in names {
-        let ws = name.chars().map(|ch| ch as usize - 64).sum();
-        if tri_numbers.contains(&ws) {
-            println!("{} {}", name, ws);
+        let word_score = name.chars().map(|ch| ch as usize - 64).sum();
+        if tri_numbers.contains(&word_score) {
+            println!("{} {}", name, word_score);
             count += 1;
         }
     }
@@ -27,18 +26,20 @@ fn main() {
 // 162
 
 //移除引号
+/*
 fn remove_quote(s: &str) -> String {
     s.chars().filter(|c| *c != '"').collect()
 }
+*/
 
 // 单词在字母表中分数
 //fn word_score(word: &str) -> usize {
 //    word.chars().map(|ch| ch as usize - 64).sum()
-    /*let mut score = 0;
-    for ch in word.chars() {
-        score += letter_number(ch) as usize;
-    }
-    score */
+/*let mut score = 0;
+for ch in word.chars() {
+    score += letter_number(ch) as usize;
+}
+score */
 //}
 
 /*
