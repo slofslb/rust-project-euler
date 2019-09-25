d28abf729e35a7766bfa1c20294a039efd8dd6bd
Shen Longbin <slofslb@qq.com>
2019-09-25 09:46:09 +0800

    精简代码

--- euler42/src/main.rs
+++ euler42/src/main.rs
@@ -4,12 +4,13 @@
 fn main() {
     // 准备足够的三角数
     let mut tri_numbers = vec![];
-    for i in 1..100 {
+    for i in 1..=100 {
         tri_numbers.push(i * (i + 1) / 2);
     }
-    println!("{:?}", tri_numbers);
+    //println!("{:?}", tri_numbers);
 
     let data = std::fs::read_to_string("words.txt").expect("读文件失败");
+    // 删除引号
     let data2: String = data.chars().filter(|c| *c != '"').collect();
     let names: Vec<&str> = data2.split(",").collect();
 
@@ -17,37 +18,10 @@ fn main() {
     for name in names {
         let word_score = name.chars().map(|ch| ch as usize - 64).sum();
         if tri_numbers.contains(&word_score) {
-            println!("{} {}", name, word_score);
+            //println!("{} {}", name, word_score);
             count += 1;
         }
     }
-    println!("\n{}", count);
+    println!("{}", count);
 }
 // 162
-
-//移除引号
-/*
-fn remove_quote(s: &str) -> String {
-    s.chars().filter(|c| *c != '"').collect()
-}
-*/
-
-// 单词在字母表中分数
-//fn word_score(word: &str) -> usize {
-//    word.chars().map(|ch| ch as usize - 64).sum()
-/*let mut score = 0;
-for ch in word.chars() {
-    score += letter_number(ch) as usize;
-}
-score */
-//}
-
-/*
-// 一个字符在字母表中分数，'A' -> 1，'B' -> 2
-fn letter_number(ch: char) -> u8 {
-    // 字母A的ASCII编码为65
-    (ch as u8) - 64
-    //let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    //letters.chars().position(|c| c == ch).unwrap() + 1
-}
-*/
