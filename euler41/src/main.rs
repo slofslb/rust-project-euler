4a6b8e6206402c66b39f6630583f5158aa6c7ff8
Shen Longbin <slofslb@qq.com>
2019-09-25 10:00:20 +0800

    permutohedron

--- euler41/src/main.rs
+++ euler41/src/main.rs
@@ -1,17 +1,18 @@
-// 借鉴第24题，再改为itertools的实现
-use itertools::Itertools;
+// 借鉴第24题，再改为依赖第三方的排列组合库来实现
+use permutohedron::heap_recursive;
 
 fn main() {
-    let it = (1..=7).combinations(7);
     let mut max_prime = 0;
-    for v in it {
+    let mut data = [1, 2, 3, 4, 5, 6, 7];
+    heap_recursive(&mut data, |permutation| {
+        let v = permutation.to_vec();
+        // println!("{:?}", v);
         // vec![1, 2, 3, 4, 5, 6, 7] -> 1234567
-        println!("{:?}", v);
         let d = v.iter().fold(0, |x, a| 10 * x + a);
         if primes::is_prime(d as u64) && d > max_prime {
             println!("{}", d);
             max_prime = d;
         }
-    }
+    })
 }
 // 7652413
