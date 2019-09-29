a463a2244db5e92d2e3962b41f88144ca61151c0
shen longbin <slofslb@qq.com>
2019-09-29 23:16:51 +0800

    comment

--- euler49/src/main.rs
+++ euler49/src/main.rs
@@ -1,36 +1,34 @@
 use permutohedron::heap_recursive;
 
 fn main() {
-    for x in 1000u64..9999 {
+    for x in 1000u64..=9999 {
         if !primes::is_prime(x) {
             continue;
         }
+        // 拆成4个数字
         let mut vx: Vec<u64> = x
             .to_string()
             .chars()
             .map(|c| c.to_digit(10).unwrap() as u64)
             .collect();
-        //println!("{:?}", vx);
 
-        let mut ys = Vec::new();
+        let mut vy = Vec::new();
         heap_recursive(&mut vx, |pt| {
-            let perm_x = pt
-                .to_vec()
-                .iter()
-                .map(|p| p.to_string())
-                .collect::<String>()
-                .parse::<u64>()
-                .unwrap();
-            ys.push(perm_x)
+            let y = pt.to_vec().iter().fold(0, |x, a| 10 * x + a);
+            if y > x && primes::is_prime(y) && !vy.contains(&y) {
+                vy.push(y);
+            }
         });
-        //println!("{:?}", ys);
+        //
+        if vy.len() >= 2 {
+            println!("{} {:?}", x, vy);
+        }
 
-        for &y in ys.iter() {
-            if y > x && primes::is_prime(y) {
-                let z = (y - x) + y;
-                if ys.contains(&z) && primes::is_prime(z) {
-                    println!("{} {} {}", x, y, z);
-                }
+        for &y in vy.iter() {
+            // 按等差关系形成第三个数
+            let z = (y - x) + y;
+            if vy.contains(&z) && primes::is_prime(z) {
+                println!("{}{}{}", x, y, z);
             }
         }
     }
