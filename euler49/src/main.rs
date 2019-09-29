61e04716969905e6fe4de83558c2e33b060e1efd
shen longbin <slofslb@qq.com>
2019-09-29 22:43:18 +0800

    用primes函数库

--- euler49/src/main.rs
+++ euler49/src/main.rs
@@ -1,18 +1,14 @@
 use permutohedron::heap_recursive;
 
 fn main() {
-    let max_number_to_check = 10000;
-    let mut prime_mask = vec![true; max_number_to_check];
-    fill_prime_mask(&mut prime_mask);
-
-    for x in 1000u32..9999 {
-        if !prime_mask[x as usize] {
+    for x in 1000u64..9999 {
+        if !primes::is_prime(x) {
             continue;
         }
-        let mut vx: Vec<u32> = x
+        let mut vx: Vec<u64> = x
             .to_string()
             .chars()
-            .map(|c| c.to_digit(10).unwrap())
+            .map(|c| c.to_digit(10).unwrap() as u64)
             .collect();
         //println!("{:?}", vx);
 
@@ -23,34 +19,18 @@ fn main() {
                 .iter()
                 .map(|p| p.to_string())
                 .collect::<String>()
-                .parse::<u32>()
+                .parse::<u64>()
                 .unwrap();
             ys.push(perm_x)
         });
         //println!("{:?}", ys);
 
         for &y in ys.iter() {
-            if y > x && prime_mask[y as usize] {
+            if y > x && primes::is_prime(y) {
                 let z = (y - x) + y;
-                if ys.contains(&z) && prime_mask[z as usize] {
+                if ys.contains(&z) && primes::is_prime(z) {
                     println!("{} {} {}", x, y, z);
                 }
-            }
-        }
-    }
-}
-
-fn fill_prime_mask(prime_mask: &mut [bool]) {
-    prime_mask[0] = false;
-    prime_mask[1] = false;
-
-    const FIRST_PRIME_NUMBER: usize = 2;
-    for p in FIRST_PRIME_NUMBER..prime_mask.len() {
-        if prime_mask[p] {
-            let mut i = 2 * p;
-            while i < prime_mask.len() {
-                prime_mask[i] = false;
-                i += p;
             }
         }
     }
