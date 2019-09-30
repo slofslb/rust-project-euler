491f295e249eed10f58fc07a8e4cb0d463f1e8f2
shen longbin <slofslb@qq.com>
2019-09-30 22:31:57 +0800

    off 1 bug

--- euler57/src/main.rs
+++ euler57/src/main.rs
@@ -5,13 +5,13 @@ fn main() {
     let mut count = 0;
     let mut a = BigUint::from(3 as u64);
     let mut b = BigUint::from(2 as u64);
-    for _i in 1..=1000 {
+    for _i in 2..=10 {
         let c = &a + &b;
         a = &c + &b;
         b = c;
+            println!("{} {}", a, b);
         if a.to_string().len() > b.to_string().len() {
             count += 1;
-            //println!("{} {}", a, b);
         }
     }
     println!("{}", count);
