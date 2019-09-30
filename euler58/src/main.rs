8f9521f18a43aab8631b5bfc5a28531fa4d460fc
shen longbin <slofslb@qq.com>
2019-09-30 22:54:47 +0800

    add a var, remove a comment

--- euler58/src/main.rs
+++ euler58/src/main.rs
@@ -7,10 +7,10 @@ fn main() {
     let mut count_prime = 0;
     for n in (3..).step_by(2) {
         let lower_right = n * n;
-        count_prime += (0..4)
-            .map(|x| lower_right - (n - 1) * x) // 四个对象线上的数字
-            .filter(|&x| pset.is_prime(x))
-            .count();
+        let prime_four_corner = (0..4)
+            .map(|x| lower_right - (n - 1) * x) 
+            .filter(|&x| pset.is_prime(x));
+        count_prime += prime_four_corner.count();
 
         let percent = (count_prime as f32) / ((2 * n - 1) as f32);
         if percent < 0.1 {
