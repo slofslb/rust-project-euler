c8b82c0e007195259082658f69f69a18ef97b152
shen longbin <slofslb@qq.com>
2019-10-01 17:54:23 +0800

    考虑了退出条件

--- euler63/src/main.rs
+++ euler63/src/main.rs
@@ -3,17 +3,23 @@ use num_bigint::BigUint;
 
 fn main() {
     let mut count = 0;
-    for b in 1..100 {
-        for a in 1..=9 {
+    for a in 1..=9 {
+        for b in 1.. {
             let p = power(a, b).to_string();
+            //println!("{} ^ {} = {} {}", a, b, p, p.len());
+            if p.len() < b as usize {
+                // 位数永远不可能超过幂次了，退出内层循环
+                break;
+            }
             if p.len() == b as usize {
                 count += 1;
-                println!("{} {} {} {}", count, a, b, p);
+                println!("{}:  {} ^ {} = {}", count, a, b, p);
             }
         }
     }
     println!("{}", count);
 }
+// 49
 
 fn power(a: u64, b: u64) -> BigUint {
     let mut p = BigUint::from(a);
