76c4721445ef91c3483bd0efae5af90e0ecfc86d
shen longbin <slofslb@qq.com>
2019-10-02 15:00:34 +0800

    写得清楚一些

--- euler206/src/main.rs
+++ euler206/src/main.rs
@@ -2,24 +2,29 @@ fn main() {
     let start = 1020304050607080900.0_f64.sqrt() as u64;
     let end = 1929394959697989990.0_f64.sqrt() as u64;
     for i in start..=end {
-        if i % 10 != 0 { //最后1位必须是0
+        if i % 10 != 0 {
+            //最后1位必须是0
             continue;
-        } 
+        }
         let m = i * i;
-        let chars: Vec<u32> = m
-            .to_string()
-            .chars()
-            .map(|c| c.to_digit(10).unwrap())
-            .collect();
-
-        for j in 0..9 {
-            if chars[j * 2] != (j + 1) as u32 {
-                break;
-            }
-            if j == 8 {
-                println!("{} {}", i, m);
-                return;
-            }
+        if meet_cond(m) {
+            println!("{} {}", i, m);
+            break;
         }
     }
+}
+
+fn meet_cond(m: u64) -> bool {
+    let chars: Vec<u32> = m
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    for j in 0..9 {
+        if chars[j * 2] != (j + 1) as u32 {
+            return false;
+        }
+    }
+    true
 }
