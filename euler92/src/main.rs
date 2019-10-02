f11cd5ad252addb9f018e0fbd6af4c0e03a38708
shen longbin <slofslb@qq.com>
2019-10-02 14:33:51 +0800

    u64

--- euler92/src/main.rs
+++ euler92/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("{}", count);
 }
 
-fn square_chain_arrive(n: u32) -> u32 {
+fn square_chain_arrive(n: u64) -> u64 {
     let mut x = n;
     while x != 1 && x != 89 {
         x = square_sum(x);
@@ -16,9 +16,9 @@ fn square_chain_arrive(n: u32) -> u32 {
     x
 }
 
-fn square_sum(n: u32) -> u32 {
+fn square_sum(n: u64) -> u64 {
     n.to_string()
         .chars()
-        .map(|x| x.to_digit(10).unwrap().pow(2))
-        .sum::<u32>()
+        .map(|x| x.to_digit(10).unwrap().pow(2) as u64)
+        .sum::<u64>()
 }
--- euler92/target/release/euler92.d
+++
@@ -1,1 +0,0 @@
-C:\Users\shenlb\rust-project-euler\euler92\target\release\euler92.pdb: C:\Users\shenlb\rust-project-euler\euler92\src\main.rs
