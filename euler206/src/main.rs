4f990a168b5a40c2d2245b45ff1b5f35ed9e5422
shen longbin <slofslb@qq.com>
2019-10-02 15:02:27 +0800

    快一点

--- euler206/src/main.rs
+++ euler206/src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let start = 1020304050607080900.0_f64.sqrt() as u64;
+    let start = (1020304050607080900.0_f64.sqrt() as u64) / 10 * 10;
     let end = 1929394959697989990.0_f64.sqrt() as u64;
-    for i in start..=end {
-        if i % 10 != 0 {
-            //最后1位必须是0
-            continue;
-        }
+    for i in (start..=end).step_by(10) {
         let m = i * i;
         if meet_cond(m) {
             println!("{} {}", i, m);
