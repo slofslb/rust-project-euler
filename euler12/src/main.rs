5321ed03268c30843bad44488e33d931fb796f28
shen longbin <slofslb@qq.com>
2019-10-06 14:50:45 +0800

    fmt

--- euler12/src/main.rs
+++ euler12/src/main.rs
@@ -1,19 +1,24 @@
 fn main() {
-    for i in 2.. { // 12375
+    for i in 2.. {
         let num = (1..=i).sum::<u32>();
         let f = factors(num);
         if f.len() * 2 > 500 {
-            println!("i:{} num:{} len:{} half of factors:{:?}", i, num, 2 * f.len(), f );
-            println!("{}", num );
+            println!(
+                "i:{} num:{} len:{} half of factors:{:?}",
+                i,
+                num,
+                2 * f.len(),
+                f
+            );
+            println!("{}", num);
             break;
         }
-
-    }    
+    }
 }
 // 76576500
 
 // 只求一半的因子就行，可以大幅提高速度
-fn factors(num :u32) -> Vec<u32> {
+fn factors(num: u32) -> Vec<u32> {
     let s = (num as f32).sqrt() as u32;
-    (1..=s).filter(|x| num % x == 0).collect::<Vec<u32>>() 
+    (1..=s).filter(|x| num % x == 0).collect::<Vec<u32>>()
 }
