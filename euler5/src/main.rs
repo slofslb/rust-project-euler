0a144adac631a3d49252c916125a96a344c6dc74
shen longbin <slofslb@qq.com>
2019-10-06 19:26:22 +0800

    用for语句

--- euler5/src/main.rs
+++ euler5/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let mut x = 100;
-    'outer: loop {
+    'outer: for x in (100..).step_by(2) {
         for f in (2..=20).rev() {
-            if x % f != 0 {break;}
+            if x % f != 0 {
+                break;
+            }
             if f == 2 {
                 println!("{}", x);
                 break 'outer;
             }
         }
-        x += 2;
     }
 }
 // 232792560
