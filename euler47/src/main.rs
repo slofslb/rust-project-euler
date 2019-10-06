7565dbaed7a08a114687ced7aeb72ba3fa2f609d
shen longbin <slofslb@qq.com>
2019-10-06 15:23:35 +0800

    改变量名

--- euler47/src/main.rs
+++ euler47/src/main.rs
@@ -1,18 +1,18 @@
 // 有许多重复计算，没做优化
 fn main() {
     for n in 2.. {
-        if meet_cond(n) {
+        if has_four_factors_uniq(n) {
             println!("{}", n);
             break;
         }
     }
     // 另一种写法
-    let n = (2..).filter(|x| meet_cond(*x)).next().unwrap();
+    let n = (2..).filter(|x| has_four_factors_uniq(*x)).next().unwrap();
     println!("{:?}", n);
 }
 // 134043
 
-fn meet_cond(n: u64) -> bool {
+fn has_four_factors_uniq(n: u64) -> bool {
     let xf = primes::factors_uniq(n);
     if xf.len() != 4 {
         return false;
