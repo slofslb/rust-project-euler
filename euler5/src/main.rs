ebdbc46114931e706c195d5ccac4ab9794091f38
Shen Longbin <slofslb@qq.com>
2019-10-08 21:41:46 +0800

    增加2种写法

--- euler5/src/main.rs
+++ euler5/src/main.rs
@@ -10,5 +10,25 @@ fn main() {
             }
         }
     }
+
+    // 另一种写法
+    for x in (100..).step_by(2) {
+        if can_divide_1_to_20(x) {
+            println!("{}", x);
+            break;
+        }
+    }
+
+    // 还可以这样写
+    print!("{}", (100..).step_by(2).filter(|&x| can_divide_1_to_20(x)).next().unwrap());
 }
 // 232792560
+
+fn can_divide_1_to_20(x: u64) -> bool {
+    for f in (2..=20).rev() {
+        if x % f != 0 {
+            return false;
+        }
+    }
+    true
+}
