d9774854d5af171a29efad92c087213926be9ab9
Shen Longbin <slofslb@qq.com>
2019-10-27 22:29:02 +0800

    还是慢

--- euler73/src/main.rs
+++ euler73/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut count = 0;
     let mut v:Vec<(u64, u64)> = vec![];
-    for d in 2..=100 {
+    for d in 2..=1000 {
         for n in 1..d {
             if 2 * n < d && d < 3 * n {
                 let r = gcd(n, d);
@@ -17,12 +17,6 @@ fn main() {
 }
 
 fn gcd(origin_a:u64, origin_b:u64) -> (u64, u64) {
-    let mut a = origin_a;
-    let mut b = origin_b;
-    while b != 0 {
-		let t = a % b;
-		a = b;
-		b = t;
-    }
-    (origin_a / a, origin_b / a)
+    let g = num::integer::gcd(origin_a, origin_b);
+    (origin_a / g, origin_b / g)
 } 
