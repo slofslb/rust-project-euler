564856047b271959cbe9fe2efd05cde39ccca429
Shen Longbin <slofslb@qq.com>
2019-10-27 21:57:07 +0800

    效率很差

--- euler73/src/main.rs
+++ euler73/src/main.rs
@@ -1,13 +1,28 @@
 fn main() {
     let mut count = 0;
-    for d in 2..=8 { 
+    let mut v:Vec<(u64, u64)> = vec![];
+    for d in 2..=100 {
         for n in 1..d {
-            let r = (n as f64) / (d as f64);
-            if  1.0 / 3.0 < r && r < 1.0 / 2.0 {
-                println!("{} / {}", n, d);
-                count += 1;
+            if 2 * n < d && d < 3 * n {
+                let r = gcd(n, d);
+                if !v.contains(&r) {
+                    v.push(r);
+                    //println!("{:?} ", r);
+                    count += 1;
+                }
             }
         }
     }
     println!("{}", count);
 }
+
+fn gcd(origin_a:u64, origin_b:u64) -> (u64, u64) {
+    let mut a = origin_a;
+    let mut b = origin_b;
+    while b != 0 {
+		let t = a % b;
+		a = b;
+		b = t;
+    }
+    (origin_a / a, origin_b / a)
+} 
