f6b972b485637068c3a448f9c53b49ce0c8b4404
shen longbin <slofslb@qq.com>
2019-12-21 06:56:25 +0800

    finish, some minutes

--- euler100/src/main.rs
+++ euler100/src/main.rs
@@ -1,18 +1,19 @@
 fn main() {
     let sqrt_1_2 = 0.5_f64.sqrt();
-    for n in 2..10_000_000 {
+    for n in 1000_000_000_000_u64.. {
         let b = (sqrt_1_2 * (n as f64) + (1.0 - sqrt_1_2)) as u64;
         if n * (n - 1) == 2 * b * (b - 1) {
             println!("n:{} b:{}", n, b);
+            break;
         }
     }
 
-    // main1();
+    main1();
 }
 
 
 fn main1() {
-    for n in 2..200_000_u64 {
+    for n in 2..100_000_u64 {
         let temp = n * (n-1) / 2;
         for b in 2..n {
             if temp == b * (b - 1) {
