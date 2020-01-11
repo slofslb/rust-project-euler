ee23d425237632ab4a723a9256b19b6e44a81e99
shen longbin <slofslb@qq.com>
2020-01-11 18:40:35 +0800

    solve S(100)

--- euler650/src/main.rs
+++ euler650/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     //println!("{:?}", fact);
 
     let mut s = 0;
-    for n in 1..=10 {
+    for n in 10..=10 {
         let mut b = BigUint::from(1_u64);
         for r in 1..=n {
             let comb = &fact[n] / &fact[r] / &fact[n - r];
@@ -22,10 +22,12 @@ fn main() {
         let bb = b.to_string().parse::<u64>().unwrap();
         //println!("B({}) = {}", n, bb);
 
-        let f_all = primes::factors(bb);
+        let mut f_all = primes::factors(bb);
         let f_uniq = primes::factors_uniq(bb);
-        //println!("{:?}", f_all);
-        //println!("{:?}", f_uniq);
+        println!("{:?}", f_all);
+        println!("{:?}", f_uniq);
+        vec_remove(&mut f_all, &f_uniq);
+        println!("{:?}", f_all);
 
         let mut d = 1;
         for f in f_uniq {
@@ -37,4 +39,85 @@ fn main() {
         s += d;
         println!("S({}) = {}", n, s);
     }
+
+    let rrr = factors_b(5);
+    println!("{:?}", rrr);
+
+    let temp = factors_sum(&rrr);
+    println!("{:?}", temp);
+
+    let mut s = 0;
+    for n in 1..=100 {
+        let rrr = factors_b(n);
+        //println!("{:?}", rrr);
+    
+        let temp = factors_sum(&rrr);
+        println!("D({}) = {:?}", n, temp);
+
+        s = (s + temp) % 1_000_000_007_u64;
+        println!("S({}) = {}", n, s);
+    }    
+}
+
+fn factors_sum(v: &Vec<u64>) -> u64 {
+    let mut uniq = v.clone();
+    uniq.dedup();
+
+    let mut prod = BigUint::from(1_u64);
+    for p in uniq {
+        let c = v.iter().filter(|&x| *x == p).count() as u64;
+        let t = (big_pow(p, c+1) - BigUint::from(1_u64)) / (BigUint::from(p-1));
+        //println!("{} {} {}", p, c, t);
+        prod = (prod * t);// % 1_000_000_007_u64;
+    }
+    let prod = prod % 1_000_000_007_u64;
+    prod.to_string().parse::<u64>().unwrap()
+}
+
+fn big_pow(a:u64, b:u64) -> BigUint {
+    let mut prod = BigUint::from(1 as u64);
+    for _i in 0..b {
+        prod *= BigUint::from(a as u64);
+    }
+    prod
+}
+
+
+
+fn factors_b(n:u64) -> Vec<u64>{
+    let mut factors = vec![];
+    for i in 1..=n {
+        let mut f = comb_factors(n, i);
+        factors.append(&mut f);
+    }
+    factors.sort();
+    factors.to_vec()
+}
+
+
+// va中元素已经排好序
+fn vec_remove(va: &mut Vec<u64>, vb:&Vec<u64>){
+    for item in vb {
+        let index = va.binary_search(&item).unwrap();
+        //println!("{:?} {:?} {}", va, vb, index);
+        va.remove(index);
+    }
+}
+
+fn comb_factors(m:u64, n:u64) -> Vec<u64> {
+    let mut factors = vec![];
+    let mut x = m;
+    for i in 0..n {
+        let mut f = primes::factors(x);
+        factors.append(&mut f);
+        x -= 1;
+    }
+    factors.sort();
+    //println!("{:?}", factors);
+    for i in 2..=n {
+        let f = primes::factors(i);
+        //println!("{} {:?}", n, f);
+        vec_remove(&mut factors, &f);
+    }
+    factors.to_vec()
 }
