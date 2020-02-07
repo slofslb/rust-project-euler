f497efaf2d6e936283a53ce99b3c5ad65de656b9
shen longbin <slofslb@qq.com>
2020-02-07 18:57:06 +0800

    solve

--- euler345/src/main.rs
+++ euler345/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    let mut cache = vec![0; 2_usize.pow(5)];
     let matrix = vec![
         7, 53, 183, 439, 863, // row 1
         497, 383, 563, 79, 973, // row 2
@@ -6,8 +7,10 @@ fn main() {
         627, 343, 773, 959, 943, // row 4
         767, 473, 103, 699, 303,
     ];
-    println!("{}", matrix_sum(&matrix));
+    let mut p = vec![];
+    assert_eq!(3315, matrix_sum(&mut cache, &matrix, &mut p));
 
+    let mut cache = vec![0; 2_usize.pow(15)];
     let matrix = vec![
         7, 53, 183, 439, 863, 497, 383, 563, 79, 973, 287, 63, 343, 169, 583, 627, 343, 773, 959,
         943, 767, 473, 103, 699, 303, 957, 703, 583, 639, 913, 447, 283, 463, 29, 23, 487, 463,
@@ -23,29 +26,72 @@ fn main() {
         586, 966, 232, 308, 833, 251, 631, 107, 813, 883, 451, 509, 615, 77, 281, 613, 459, 205,
         380, 274, 302, 35, 805,
     ];
-    println!("{}", matrix_sum(&matrix));
+    println!("{}", matrix_sum(&mut cache, &matrix, &mut p));
+
+    // ugly to find the positions!
+    let mut p = vec![];
+    for j in 0..15 {
+        let mut index = 0;
+        let mut mmax = 0;
+        for i in 0..15_usize {
+            if !p.contains(&i) {
+                p.push(i);
+                let hash = hash_code(&p);
+                if cache[hash] == 0 {
+                    println!("no cache");
+                    index = i;
+                }
+                if cache[hash] > mmax {
+                    mmax = cache[hash];
+                    index = i;
+                    //println!("{:?} {}", p, cache[hash]);
+                }
+                p.pop();
+            }
+        }
+        p.push(index);
+        println!("{:?}", p);
+    }
+}
+// 13938
+
+fn matrix_sum(cache: &mut [usize], v: &Vec<usize>, p: &mut Vec<usize>) -> usize {
+    let r = (v.len() as f64).sqrt() as usize;
+    let col = p.len();
+    if col == r {
+        return 0;
+    }
+
+    let hash = hash_code(p);
+    //println!("{:?} {}", &p, hash);
+    if cache[hash] > 0 {
+        if p.len() < 2 {
+            //println!("{:?}", p);
+        }
+        return cache[hash];
+    }
+
+    let mut max_sum = 0;
+    for row in 0..r {
+        let index = row * r + col;
+        if !p.contains(&row) {
+            let mut p2 = p.clone();
+            p2.push(row);
+            let temp = v[index] + matrix_sum(cache, &v, &mut p2);
+            if temp > max_sum {
+                max_sum = temp;
+            }
+        }
+    }
+    cache[hash] = max_sum;
+    //println!("{:?} {} {}", &p, hash, max_sum);
+    max_sum
 }
 
-fn matrix_sum(v: &Vec<i64>) -> i64 {
-    let mut max_sum = 0;
-    if v.len() == 1 {
-        return v[0];
+fn hash_code(p: &Vec<usize>) -> usize {
+    let mut hash = 0;
+    for element in p.iter() {
+        hash += 1 << *element;
     }
-    let r = (v.len() as f64).sqrt() as usize;
-    for i in (0..v.len()).step_by(r) {
-        let mut v2 = v.clone();
-        for j in (0..v.len()).step_by(r) {
-            v2[j] = -1;
-        }
-        for j in 0..r {
-            v2[i + j] = -1;
-        }
-        v2 = v2.into_iter().filter(|&x| x >= 0).collect::<Vec<_>>();
-        let s = v[i] + matrix_sum(&v2);
-        if s > max_sum {
-            max_sum = s;
-            //println!("{} {:?}", v[i], v2);
-        }
-    }
-    max_sum
+    hash
 }
