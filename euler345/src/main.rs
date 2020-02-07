d5ccd8f37d05013ec8098bef6fe255e04c5e645e
shen longbin <slofslb@qq.com>
2020-02-07 23:07:54 +0800

    整理代码

--- euler345/src/main.rs
+++ euler345/src/main.rs
@@ -1,18 +1,16 @@
 fn main() {
+    let mat = vec![
+        7, 53, 183, 439, 863, // row 0
+        497, 383, 563, 79, 973, // row 1
+        287, 63, 343, 169, 583, // row 2
+        627, 343, 773, 959, 943, // row 3
+        767, 473, 103, 699, 303, // row 4
+        ];
     let mut cache = vec![0; 2_usize.pow(5)];
-    let matrix = vec![
-        7, 53, 183, 439, 863, // row 1
-        497, 383, 563, 79, 973, // row 2
-        287, 63, 343, 169, 583, // row 3
-        627, 343, 773, 959, 943, // row 4
-        767, 473, 103, 699, 303,
-    ];
-    let mut p = vec![];
-    assert_eq!(3315, matrix_sum(&mut cache, &matrix, &mut p));
+    let mut path = vec![];
+    assert_eq!(3315, matrix_sum(&mut cache, &mat, &mut path));
 
-    let mut cache = vec![0; 2_usize.pow(15)];
-    let mut p = vec![];
-    let matrix = vec![
+    let mat = vec![
         7, 53, 183, 439, 863, 497, 383, 563, 79, 973, 287, 63, 343, 169, 583, 627, 343, 773, 959,
         943, 767, 473, 103, 699, 303, 957, 703, 583, 639, 913, 447, 283, 463, 29, 23, 487, 463,
         993, 119, 883, 327, 493, 423, 159, 743, 217, 623, 3, 399, 853, 407, 103, 983, 89, 463, 290,
@@ -26,72 +24,67 @@ fn main() {
         450, 476, 712, 914, 838, 669, 875, 299, 823, 329, 699, 815, 559, 813, 459, 522, 788, 168,
         586, 966, 232, 308, 833, 251, 631, 107, 813, 883, 451, 509, 615, 77, 281, 613, 459, 205,
         380, 274, 302, 35, 805,
-    ];
-    println!("{}", matrix_sum(&mut cache, &matrix, &mut p));
+        ];
+    let mut cache = vec![0; 2_usize.pow(15)];
+    let mut path = vec![];
+    println!("{}", matrix_sum(&mut cache, &mat, &mut path));
 
-    // ugly to find the positions!
-    let mut p = vec![];
-    for col in 0..15 {
-        let mut index = 0;
-        let mut mmax = 0;
-        for i in 0..15_usize {
-            if !p.contains(&i) {
-                p.push(i);
-                let hash = hash_code(&p);
-                if cache[hash] == 0 {
-                    println!("no cache");
-                    index = i;
+    // ugly codes to output the path!
+    let num_cols = 15_usize;
+    let mut path = vec![];
+    for col in 0..num_cols {
+        let mut selected_row = 999;
+        let mut max = 0;
+        for row in 0..num_cols {
+            if !path.contains(&row) {
+                path.push(row);
+                let hash = hash_code(&path);
+                if mat[row * num_cols + col] + cache[hash] > max {
+                    max = mat[row * num_cols + col] + cache[hash];
+                    selected_row = row;
                 }
-                if matrix[i*15+col] + cache[hash] >= mmax {
-                    mmax = matrix[i*15+col] + cache[hash];
-                    index = i;
-                    println!("{:?} {}", p, cache[hash]);
-                }
-                p.pop();
+                path.pop();
             }
         }
-        p.push(index);
-        println!("{:?}", p);
+        path.push(selected_row);
     }
+    println!("path: {:?}", path);
 }
 // 13938
 
-fn matrix_sum(cache: &mut [usize], v: &Vec<usize>, p: &mut Vec<usize>) -> usize {
-    let r = (v.len() as f64).sqrt() as usize;
-    let col = p.len();
-    if col == r {
+fn matrix_sum(cache: &mut [usize], mat: &Vec<usize>, path: &mut Vec<usize>) -> usize {
+    // 总列数
+    let num_cols = (mat.len() as f64).sqrt() as usize;
+    // 准备填充的列号
+    let cur_col = path.len();
+    if cur_col == num_cols {
         return 0;
     }
 
-    let hash = hash_code(p);
-    //println!("{:?} {}", &p, hash);
+    let hash = hash_code(path);
     if cache[hash] > 0 {
-        if p.len() < 2 {
-            //println!("{:?}", p);
-        }
         return cache[hash];
     }
 
     let mut max_sum = 0;
-    for row in 0..r {
-        let index = row * r + col;
-        if !p.contains(&row) {
-            let mut p2 = p.clone();
-            p2.push(row);
-            let temp = v[index] + matrix_sum(cache, &v, &mut p2);
+    for row in 0..num_cols {
+        if !path.contains(&row) {
+            path.push(row);
+            let temp = mat[row * num_cols + cur_col] + matrix_sum(cache, &mat, path);
             if temp > max_sum {
                 max_sum = temp;
             }
+            path.pop();
         }
     }
     cache[hash] = max_sum;
-    //println!("{:?} {} {}", &p, hash, max_sum);
+    //println!("{:?} {} {}", &path, hash, max_sum);
     max_sum
 }
 
-fn hash_code(p: &Vec<usize>) -> usize {
+fn hash_code(path: &Vec<usize>) -> usize {
     let mut hash = 0;
-    for element in p.iter() {
+    for element in path.iter() {
         hash += 1 << *element;
     }
     hash
