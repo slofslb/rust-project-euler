a058ca0be99ed39a312d723063e5f11f66dbfa32
shen longbin <slofslb@qq.com>
2020-02-09 11:41:47 +0800

    clippy

--- euler3/src/main.rs
+++ euler3/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let big_num = 600851475143;
+    let big_num = 600_851_475_143;
     let mut i = (big_num as f64).sqrt() as u64;
     while i > 0 {
         if big_num % i == 0 && is_prime(i) {
@@ -12,12 +12,14 @@ fn main() {
         println!("not found");
     }
     // 另一种方法，用primes函数库
-    println!("{:?}", primes::factors_uniq(600851475143).last().unwrap());
+    println!("{:?}", primes::factors_uniq(600_851_475_143).last().unwrap());
+
+    //main_slow();
 }
 // 6857
 
 fn is_prime(num: u64) -> bool {
-    for i in 2..(num / 2 + 1) {
+    for i in 2..=num / 2 {
         if num % i == 0 {
             return false;
         }
@@ -26,7 +28,7 @@ fn is_prime(num: u64) -> bool {
 }
 
 fn main_slow() {
-    let big_num = 600851475143;
+    let big_num = 600_851_475_143;
     for i in (1..=big_num / 2).rev() {
         if big_num % i == 0 && is_prime(i) {
             println!("{}", i);
