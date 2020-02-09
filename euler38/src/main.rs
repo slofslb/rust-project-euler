135772c1190f7920068e023575fd178259821ec9
shen longbin <slofslb@qq.com>
2020-02-09 13:19:26 +0800

    version 混乱了

--- euler38/src/main.rs
+++ euler38/src/main.rs
@@ -3,23 +3,14 @@ fn main() {
     let mut max = "".to_string();
     for n in 1..=9999 {
         let mut s = String::from("");
-<<<<<<< HEAD
         for i in 1..=9 {
             let prod = n * i;    
-=======
-        for n in 1..=9 {
-            let prod = a * n;
->>>>>>> d9774854d5af171a29efad92c087213926be9ab9
             s.push_str(&prod.to_string());
             if !exists_only_once_1_to_9(&s) {
                 break;
             }
             if s.len() == 9 && s > max {
-<<<<<<< HEAD
                 println!("{} {} {}", n, i, s);
-=======
-                println!("{} x {:?} = {}", a, [1..n], s);
->>>>>>> d9774854d5af171a29efad92c087213926be9ab9
                 max = s.clone();
             }
         }
