ffef4683a11dff8345798f9d5ae275698a342ba4
shen longbin <slofslb@qq.com>
2020-02-09 16:25:10 +0800

    or_insert()

--- euler62/src/main.rs
+++ euler62/src/main.rs
@@ -12,23 +12,24 @@ fn main() {
             .collect();
         digits.sort();
         // 反序排列之后的整数，作为哈希表的主键
+        // 例如：5027，立方之后是127035954683，然后把里面的所有数字反序排列
+        // 主键就是987655433210，反序的作用是不漏掉任何一个0
         let key = digits.iter().rev().fold(0u64, |x, a| 10 * x + a);
-        if !hash.contains_key(&key) {
-            hash.insert(key, vec![i]);
-        } else {
-            let cubic_numbers = hash.get_mut(&key).unwrap();
-            &cubic_numbers.push(i);
-            if cubic_numbers.len() == 5 {
-                println!("{:?}", cubic_numbers);
-                println!("{:?}", cubic_numbers.iter().map(|x| x.pow(3)).collect::<Vec<u64>>());
-                let temp = cubic_numbers.iter().min().unwrap().pow(3);
-                if temp < min_cube {
-                    min_cube = temp;
-                }
+        let cubic_numbers = hash.entry(key).or_insert(vec![]);
+        cubic_numbers.push(i);
+        if cubic_numbers.len() == 5 {
+            println!("{:?}", cubic_numbers);
+            println!(
+                "x^3= {:?}",
+                cubic_numbers.iter().map(|x| x.pow(3)).collect::<Vec<u64>>()
+            );
+            let temp = cubic_numbers.iter().min().unwrap().pow(3);
+            if temp < min_cube {
+                min_cube = temp;
             }
         }
     }
-    println!("{}", min_cube);
+    println!("min cube= {}", min_cube);
 }
 
 // [5027, 7061, 7202, 8288, 8384]
