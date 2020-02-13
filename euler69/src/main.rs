fa7cfd3adcb52e53eeff1f8edaf4fed261ca0cfd
shen longbin <slofslb@qq.com>
2020-02-13 16:43:29 +0800

    add comment

--- euler69/src/main.rs
+++ euler69/src/main.rs
@@ -1,8 +1,9 @@
 use primes::PrimeSet;
 
 fn main() {
-    let mut max_ratio_n_phi = 0_f32;
-    for n in 2..=3000 {
+    // 试算一些数，找找规律
+    let mut max_ratio = 0_f64;
+    for n in 2..3000 {
         let mut phi = 0;
         for i in 1..n {
             // 最大公约数为1，表示互质
@@ -10,21 +11,23 @@ fn main() {
                 phi += 1;
             }
         }
-        let ratio_n_phi = (n as f32) / (phi as f32);
-        if ratio_n_phi > max_ratio_n_phi {
-            println!("n= {}  n/phi= {}", n, ratio_n_phi);
-            max_ratio_n_phi = ratio_n_phi;
+        let ratio = (n as f64) / (phi as f64);
+        if ratio > max_ratio {
+            println!("n= {:6}  phi={:6}  n/phi= {:.4}", n, phi, ratio);
+            max_ratio = ratio;
         }
     }
+    // 2 * 3 * 5 * 7 * 11 * ... 找到最多的素数，乘积在1百万以内
     let mut pset = PrimeSet::new();
     let mut prod = 1;
     for p in pset.iter() {
-        prod *= p;
-        if prod > 1_000_000 {
+        if prod * p > 1_000_000 {
             break;
         }
-        println!("{}", prod);
+        prod *= p;
+        println!("prime={} {}", p, prod);
     }
+    println!("{}", prod);
 }
 
 fn gcd(a: u64, b: u64) -> u64 {
