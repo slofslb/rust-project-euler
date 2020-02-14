b662dc72589ec82f4ba71e8b2e03d63869564609
shen longbin <slofslb@qq.com>
2020-02-14 17:40:27 +0800

    easy

--- euler71/src/main.rs
+++ euler71/src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
-    println!("3/7= {}", 3.0 / 7.0);
     let mut max = 0.0;
-    for numerator in 2..=1_000_000 {
-        let x = numerator * 3 / 7;
-        if x * 7 == numerator * 3 {
+    let mut numerator = 0;
+    for d in 2..=1_000_000 { // 分母
+        let n = d * 3 / 7;
+        if n * 7 == d * 3 {
             continue;
         }
-        let q = (x as f64) / (numerator as f64);
+        let q = (n as f64) / (d as f64);
         if q > max {
             max = q;
-            println!("{} / {} = {}", x, numerator, q);
+            numerator = n;
+            //println!("{} / {} = {}", numerator, denom, q);
         }
     }
+    println!("{}", numerator);
 }
