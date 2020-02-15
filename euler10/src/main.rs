edf3242074f987ddc31fde1617dec0e6083701af
shen longbin <slofslb@qq.com>
2020-02-15 21:31:52 +0800

    写在一个独立的mod中

--- euler10/src/main.rs
+++ euler10/src/main.rs
@@ -1,23 +1,13 @@
 use primes::PrimeSet;
+mod myprime;
 
 fn main() {
-    let max_number_to_check = 2_000_000;
+    let prime_masks = myprime::prime_sieve(2_000_000);
 
-    let mut prime_mask = vec![true; max_number_to_check];
-    prime_mask[0] = false;
-    prime_mask[1] = false;
-
-    let mut sum: u64 = 0;
-
-    const FIRST_PRIME_NUMBER: usize = 2;
-    for p in FIRST_PRIME_NUMBER..max_number_to_check {
-        if prime_mask[p] {
-            sum += p as u64;
-            let mut i = 2 * p;
-            while i < max_number_to_check {
-                prime_mask[i] = false;
-                i += p;
-            }
+    let mut sum = 0;
+    for (i, &mask) in prime_masks.iter().enumerate() {
+        if mask {
+            sum += i as u64;
         }
     }
     println!("{}", sum);
