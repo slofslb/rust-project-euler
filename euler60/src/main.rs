9258e1c7dc96d4b2d442118f29faaada03c1a561
slofslb@qq.com <slofslb@qq.com>
2020-03-10 22:16:33 +0800

    try petgraph

--- euler60/src/main.rs
+++ euler60/src/main.rs
@@ -1,65 +1,108 @@
 use primes::PrimeSet;
+use petgraph::Graph;
+use petgraph::algo;
+
 fn main() {
-    let max = 10000;
-    let primes = vec![];
-    let p1 = prime_pairs(max, &primes);
-    for p in p1 {
-        let p2 = prime_pairs(max, &p);
-        for q in p2 {
-            let p3 = prime_pairs(max, &q);
-            for r in p3 {
-                let p4 = prime_pairs(max, &r);
-                for s in p4 {
-                    let p5 = prime_pairs(max, &s);
-                    if !p5.is_empty() {
-                        println!("{:?}", p5);
-                        println!("{:?}", p5[0].iter().sum::<u64>());
-                    }
-                }
-            }
+    // 参考这篇文章，学会了petgraph的使用
+    // https://thobbs.cz/rust-play/petgraph_review.html
+    let mut graph : Graph<&str, &str, petgraph::Directed> = Graph::new();
+    let a = graph.add_node("a");
+    let b = graph.add_node("b");
+    let c = graph.add_node("c");
+    let d = graph.add_node("d");
+    let e = graph.add_node("e");
+    graph.add_edge(a, b, "");
+    graph.add_edge(a, c, "");
+    graph.add_edge(b, d, "");
+    graph.add_edge(b, e, "");
+
+    //let mut dag = tree.clone();
+    let f = graph.add_node("f");
+    graph.add_edge(d, f, "");
+    graph.add_edge(e, f, "");
+
+    //let mut with_scc = dag.clone();
+    graph.add_edge(f, b, "");
+
+    println!("Does 'with_scc' have strongly connected components? {}", algo::is_cyclic_directed(&graph));
+    println!("Does 'dag' have strongly connected components? {}", algo::is_cyclic_directed(&graph));
+
+    // let  _condensed_graph = algo::condensation(with_scc, true);
+    // let condensed_graph = _condensed_graph.map(
+    //     |_, w| format!("{:?}", w),
+    //     |_, w| w.clone(),
+    // );
+    // println!("{:#?}", condensed_graph);
+
+
+    // let _with_scc_and_edge_weights = with_scc.clone();
+    // let with_scc_and_edge_weights = _with_scc_and_edge_weights.map(
+    //     |_, w| w.clone(),
+    //     |i, _| i.index(),
+    // );
+    // println!("{:#?}", with_scc_and_edge_weights);
+
+    let  sccs = algo::tarjan_scc(&graph);
+    for scc in sccs {
+        println!("----");
+        if scc.len() == 4 {
+        for node_id in scc {
+            println!("{:?} {}", node_id, graph.node_weight(node_id).unwrap());
         }
+    }
+    }
+
+    // g.extend_with_edges(&[
+    //     (0, 1), (0, 2), (0, 3)
+    // ]);
+    // let tt = algo::kosaraju_scc(g);
+    // println!("{:?}", tt);
+
+
+    let max = 9000;
+    let primes: Vec<Vec<u64>> = vec![vec![]];
+    let p1 = prime_pairs(max, &primes);
+    let p2 = prime_pairs(max, &p1);
+    let p3 = prime_pairs(max, &p2);
+    let p4 = prime_pairs(max, &p3);
+    let p5 = prime_pairs(max, &p4);
+    if !p5.is_empty() {
+        println!("{:?}", p5[0]);
+        println!("{:?}", p5[0].iter().sum::<u64>());
     }
 }
 // error: 129976621
 // right: 26033 = [13, 5197, 5701, 6733, 8389]
 
-fn prime_pairs(max: u64, primes_list: &[u64]) -> Vec<Vec<u64>> {
+fn prime_pairs(max: u64, list: &Vec<Vec<u64>>) -> Vec<Vec<u64>> {
     let mut pairs = vec![];
 
-    let mut pset1 = PrimeSet::new();
-    for j in pset1.iter() {
-        if j == 2 || j == 5 {
-            continue;
-        }
-        if j > max {
-            break;
-        }
-        if primes_list.is_empty() {
-            pairs.push(vec![j]);
-            continue;
-        }
-        if j <= *primes_list.last().unwrap() {
-            continue;
-        }
-        if all_primes(primes_list, j) {
-            let mut v = primes_list.to_vec();
-            v.push(j);
-            pairs.push(v);
+    for primes_list in list.iter() {
+        let mut pset1 = PrimeSet::new();
+        for j in pset1.iter() {
+            if j > max {
+                break;
+            }
+            if primes_list.is_empty() {
+                pairs.push(vec![j]);
+                continue;
+            }
+            if j <= *primes_list.last().unwrap() {
+                continue;
+            }
+            if all_primes(primes_list, j) {
+                let mut v = primes_list.to_vec();
+                v.push(j);
+                pairs.push(v);
+            }
         }
     }
     pairs
 }
 
 fn all_primes(primes_list: &[u64], b: u64) -> bool {
-    for a in primes_list.iter() {
-        let c = format!("{}{}", a, b);
-        let c = c.parse::<u64>().unwrap();
-        if !primes::is_prime(c) {
-            return false;
-        }
-        let d = format!("{}{}", b, a);
-        let d = d.parse::<u64>().unwrap();
-        if !primes::is_prime(d) {
+    for &a in primes_list.iter() {
+        if !is_concat_primes(a, b) {
             return false;
         }
     }
--- euler61/src/main.rs
+++ euler61/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
+    let mut v = vec![];
+
     let tri: Vec<u32> = (1..)
         .map(|n| n * (n + 1) / 2)
         .filter(|&n| n > 1000)
         .take_while(|&n| n < 9999)
         .collect();
     println!("triangle: {:?}", tri);
+    for &e in tri.iter() {
+        v.push((1, head(e), tail(e)));
+    }
 
     let squ: Vec<u32> = (1..)
         .map(|n| n * n)
@@ -12,6 +17,9 @@ fn main() {
         .take_while(|&n| n < 9999)
         .collect();
     println!("square: {:?}", squ);
+    for &e in squ.iter() {
+        v.push((2, head(e), tail(e)));
+    }
 
     let pen: Vec<u32> = (1..)
         .map(|n| n * (3 * n - 1) / 2)
@@ -19,6 +27,9 @@ fn main() {
         .take_while(|&n| n < 9999)
         .collect();
     println!("pentagonal: {:?}", pen);
+    for &e in pen.iter() {
+        v.push((3, head(e), tail(e)));
+    }
 
     let hex: Vec<u32> = (1..)
         .map(|n| n * (2 * n - 1))
@@ -26,6 +37,9 @@ fn main() {
         .take_while(|&n| n < 9999)
         .collect();
     println!("hexagonal: {:?}", hex);
+    for &e in hex.iter() {
+        v.push((4, head(e), tail(e)));
+    }
 
     let hep: Vec<u32> = (1..)
         .map(|n| n * (5 * n - 3) / 2)
@@ -33,6 +47,9 @@ fn main() {
         .take_while(|&n| n < 9999)
         .collect();
     println!("heptagonal: {:?}", hep);
+    for &e in hep.iter() {
+        v.push((5, head(e), tail(e)));
+    }
 
     let oct: Vec<u32> = (1..)
         .map(|n| n * (3 * n - 2))
@@ -40,14 +57,138 @@ fn main() {
         .take_while(|&n| n < 9999)
         .collect();
     println!("octagonal: {:?}", oct);
+    for &e in oct.iter() {
+        v.push((6, head(e), tail(e)));
+    }
 
-    /*
-    for n in 2..1000 {
-        let oct = n * (3 * n - 2);
-        if oct > 9999 {
+    //println!("{:?}", v);
+
+    //find(&v, 45, 10, &[1,2,3,4,5]);
+
+    let mut samples = vec![];
+    for i in 0..100 {
+        let mut sample = vec![];
+        let a = tri[rand::random::<usize>() % tri.len()];
+        sample.push(a);
+        let b = squ[rand::random::<usize>() % squ.len()];
+        sample.push(b);
+        let c = pen[rand::random::<usize>() % pen.len()];
+        sample.push(c);
+        let d = hex[rand::random::<usize>() % hex.len()];
+        sample.push(d);
+        let e = hep[rand::random::<usize>() % hep.len()];
+        sample.push(e);
+        let f = oct[rand::random::<usize>() % oct.len()];
+        sample.push(f);
+        println!("{:?}", sample);
+
+        samples.push(sample);
+    }
+
+    let reproduce_num = 16;
+    let mut max = -100;
+    for gen in 0.. {
+        for _i in 0..reproduce_num * reproduce_num {
+            let sample = &samples[rand::random::<usize>() % samples.len()];
+            let index = rand::random::<usize>() % 6;
+            let mut sample_new = sample.clone();
+            if index == 0 {
+                sample_new[index] = tri[rand::random::<usize>() % tri.len()];
+            } else if index == 1 {
+                sample_new[index] = squ[rand::random::<usize>() % squ.len()];
+            } else if index == 2 {
+                sample_new[index] = pen[rand::random::<usize>() % pen.len()];
+            } else if index == 3 {
+                sample_new[index] = hex[rand::random::<usize>() % hex.len()];
+            } else if index == 4 {
+                sample_new[index] = hep[rand::random::<usize>() % hep.len()];
+            } else {
+                sample_new[index] = oct[rand::random::<usize>() % oct.len()];
+            }
+
+            if !samples.contains(&sample_new) {
+                let ev = eval(&sample_new);
+                if ev == 12 {
+                    println!("{:?} {}", samples[0], samples[0].iter().sum::<u32>());
+                    return;
+                }
+                if ev > max {
+                    max = ev;
+                    samples.insert(0, sample_new);
+                } else if ev == max - 2 {
+                    //println!("insert ");
+                    samples.insert(reproduce_num / 4, sample_new);
+                } else if ev == max - 4 {
+                    //println!("insert ");
+                    samples.insert(reproduce_num / 2, sample_new);
+                } else {
+                    samples.push(sample_new);
+                }
+            }
+        }
+        // 只保留前面几个优质的
+        if samples.len() > reproduce_num {
+            for p in (reproduce_num..samples.len()).rev() {
+                samples.remove(p);
+            }
+        }
+        if gen % 100 == 0 {
+            println!("{:?}  {:?} {}", samples[0], samples[reproduce_num - 1], max);
+        }
+    }
+}
+//[8256, 5625, 2882, 8128, 2512, 1281]
+
+fn head(n: u32) -> u32 {
+    n / 100
+}
+
+fn tail(n: u32) -> u32 {
+    n % 100
+}
+
+fn eval(sample: &[u32]) -> i32 {
+    let mut count = 0;
+    for i in 0..6 {
+        if head(sample[i]) == 0 || tail(sample[i]) == 0 {
+            count -= 1000;
             break;
         }
-        println!("{}", oct);
+        for j in 0..6 {
+            if i == j {
+                continue;
+            }
+
+            if head(sample[i]) == tail(sample[j]) && tail(sample[i]) == head(sample[j]) {
+                count -= 100;
+            }
+            if head(sample[i]) == tail(sample[j]) {
+                count += 1;
+            }
+            if tail(sample[i]) == head(sample[j]) {
+                count += 1;
+            }
+            if head(sample[i]) == head(sample[j]) {
+                count -= 5;
+            }
+            if tail(sample[i]) == tail(sample[j]) {
+                count -= 5;
+            }
+        }
     }
-    */
+    count
 }
+/*
+fn find(v: &Vec<(u32,u32,u32)>, start: u32, end: u32, poly_type: &[u32]) -> Vec<(u32,u32,u32)> {
+    for (head, tail, type) in v.iter() {
+        if poly_type.contains(type) && head == start {
+            let t = poly_type.clone().remove(&type);
+            let result = find(v, tail, end, &t);
+            if result.len() > 0 {
+                return result;
+            }
+        }
+    }
+    vec![]
+}
+*/
