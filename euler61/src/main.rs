4a48c2cb7365dbf2a5dd7658756f14214760a6e8
slofslb@qq.com <slofslb@qq.com>
2020-03-16 20:09:20 +0800

    ok

--- euler61/src/main.rs
+++ euler61/src/main.rs
@@ -7,36 +7,27 @@ fn main() {
     let p8 = gen_poly_numbers(1000, 9999, |n| n * (3 * n - 2));
 
     for n in p8 {
-        println!("{}", n);
-        find(&[&p6, &p5, &p3, &p4, &p7], tail(n), head(n), &[]);
+        find(&[&p3, &p4, &p5, &p6, &p7], tail(n), head(n), &[]);
+    }
+}
+// [8128, 2882, 8256, 5625, 2512, 1281] 28684
+
+fn find(lists: &[&[u32]], start: u32, end: u32, found: &[u32]) {
+    if lists.is_empty() && tail(*found.last().unwrap()) == end {
+        let mut result = found.to_vec();
+        result.push(end * 100 + head(found[0]));
+        println!("{:?} {}", result, result.iter().sum::<u32>())
     }
 
-}
-//[8256, 5625, 2882, 8128, 2512, 1281]
-
-fn head(n: u32) -> u32 {
-    n / 100
-}
-
-fn tail(n: u32) -> u32 {
-    n % 100
-}
-
-fn find(lists: &[&[u32]], start:u32, end:u32,found:&[u32]) {
-    if lists.len() == 0 && tail(*found.last().unwrap()) == end {
-        println!("found: {:?} {} {}", found, start, end);
-    }
-
-    for &cur_list in lists {
+    for (i, &cur_list) in lists.iter().enumerate() {
         for &n in cur_list {
             if head(n) == start {
-                // remove cur_list
-                let  lists_copy = &lists[1..];
-                let mut found_new = found.to_vec();
-                found_new.push(n);
-                find(lists_copy, tail(n), end, &found_new);
-                //println!("{:?}", lists_copy);
-                //println!("{:?}", found_new);
+                let mut lists_copy = lists.to_vec();
+                lists_copy.remove(i);
+                let mut found_copy = found.to_vec();
+                found_copy.push(n);
+                find(&lists_copy, tail(n), end, &found_copy);
+                //println!("{:?} {:?}", lists_copy, found_new);
             }
         }
     }
@@ -48,4 +39,12 @@ fn gen_poly_numbers(start: u32, end: u32, f: fn(u32) -> u32) -> Vec<u32> {
         .filter(|&n| n >= start)
         .take_while(|&n| n <= end)
         .collect()
+}
+
+fn head(n: u32) -> u32 {
+    n / 100
+}
+
+fn tail(n: u32) -> u32 {
+    n % 100
 }
