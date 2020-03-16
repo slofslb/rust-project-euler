2c0495e6676bdc87e5089d35e06a86ef78bd0d29
Shen Longbin <slofslb@qq.com>
2020-03-16 16:43:56 +0800

    temp

--- euler61/src/main.rs
+++ euler61/src/main.rs
@@ -1,115 +1,16 @@
 fn main() {
-    let tri = gen_poly_numbers(1000, 9999, |n| n * (n + 1) / 2);
-    let sqr = gen_poly_numbers(1000, 9999, |n| n * n);
-    let pent = gen_poly_numbers(1000, 9999, |n| n * (3 * n - 1) / 2);
-    let hex = gen_poly_numbers(1000, 9999, |n| n * (2 * n - 1));
-    let hep = gen_poly_numbers(1000, 9999, |n| n * (5 * n - 3) / 2);
-    let oct = gen_poly_numbers(1000, 9999, |n| n * (3 * n - 2));
+    let p3 = gen_poly_numbers(1000, 9999, |n| n * (n + 1) / 2);
+    let p4 = gen_poly_numbers(1000, 9999, |n| n * n);
+    let p5 = gen_poly_numbers(1000, 9999, |n| n * (3 * n - 1) / 2);
+    let p6 = gen_poly_numbers(1000, 9999, |n| n * (2 * n - 1));
+    let p7 = gen_poly_numbers(1000, 9999, |n| n * (5 * n - 3) / 2);
+    let p8 = gen_poly_numbers(1000, 9999, |n| n * (3 * n - 2));
 
-    let mut v = vec![];
-    println!("triangle: {:?}", tri);
-    for &e in tri.iter() {
-        v.push((1, head(e), tail(e)));
+    for n in p8 {
+        println!("{}", n);
+        find(&[&p6, &p5, &p3, &p4, &p7], tail(n), head(n), &[]);
     }
 
-    println!("square: {:?}", sqr);
-    for &e in sqr.iter() {
-        v.push((2, head(e), tail(e)));
-    }
-
-    println!("pentagonal: {:?}", pent);
-    for &e in pent.iter() {
-        v.push((3, head(e), tail(e)));
-    }
-
-    println!("hexagonal: {:?}", hex);
-    for &e in hex.iter() {
-        v.push((4, head(e), tail(e)));
-    }
-
-    println!("heptagonal: {:?}", hep);
-    for &e in hep.iter() {
-        v.push((5, head(e), tail(e)));
-    }
-
-    println!("octagonal: {:?}", oct);
-    for &e in oct.iter() {
-        v.push((6, head(e), tail(e)));
-    }
-
-    //find(&v, 45, 10, &[1,2,3,4,5]);
-
-    let mut samples = vec![];
-    for i in 0..100 {
-        let mut sample = vec![];
-        let a = tri[rand::random::<usize>() % tri.len()];
-        sample.push(a);
-        let b = sqr[rand::random::<usize>() % sqr.len()];
-        sample.push(b);
-        let c = pent[rand::random::<usize>() % pent.len()];
-        sample.push(c);
-        let d = hex[rand::random::<usize>() % hex.len()];
-        sample.push(d);
-        let e = hep[rand::random::<usize>() % hep.len()];
-        sample.push(e);
-        let f = oct[rand::random::<usize>() % oct.len()];
-        sample.push(f);
-        println!("{:?}", sample);
-
-        samples.push(sample);
-    }
-
-    let reproduce_num = 16;
-    let mut max = -100;
-    for gen in 0.. {
-        for _i in 0..reproduce_num * reproduce_num {
-            let sample = &samples[rand::random::<usize>() % samples.len()];
-            let index = rand::random::<usize>() % 6;
-            let mut sample_new = sample.clone();
-            if index == 0 {
-                sample_new[index] = tri[rand::random::<usize>() % tri.len()];
-            } else if index == 1 {
-                sample_new[index] = sqr[rand::random::<usize>() % sqr.len()];
-            } else if index == 2 {
-                sample_new[index] = pent[rand::random::<usize>() % pent.len()];
-            } else if index == 3 {
-                sample_new[index] = hex[rand::random::<usize>() % hex.len()];
-            } else if index == 4 {
-                sample_new[index] = hep[rand::random::<usize>() % hep.len()];
-            } else {
-                sample_new[index] = oct[rand::random::<usize>() % oct.len()];
-            }
-
-            if !samples.contains(&sample_new) {
-                let ev = eval(&sample_new);
-                if ev == 12 {
-                    println!("{:?} {}", sample_new, sample_new.iter().sum::<u32>());
-                    return;
-                }
-                if ev > max {
-                    max = ev;
-                    samples.insert(0, sample_new);
-                } else if ev == max - 2 {
-                    //println!("insert ");
-                    samples.insert(reproduce_num / 4, sample_new);
-                } else if ev == max - 4 {
-                    //println!("insert ");
-                    samples.insert(reproduce_num / 2, sample_new);
-                } else {
-                    samples.push(sample_new);
-                }
-            }
-        }
-        // 只保留前面几个优质的
-        if samples.len() > reproduce_num {
-            for p in (reproduce_num..samples.len()).rev() {
-                samples.remove(p);
-            }
-        }
-        if gen % 100 == 0 {
-            println!("{:?}  {:?} {}", samples[0], samples[reproduce_num - 1], max);
-        }
-    }
 }
 //[8256, 5625, 2882, 8128, 2512, 1281]
 
@@ -121,51 +22,26 @@ fn tail(n: u32) -> u32 {
     n % 100
 }
 
-fn eval(sample: &[u32]) -> i32 {
-    let mut count = 0;
-    for i in 0..6 {
-        if head(sample[i]) == 0 || tail(sample[i]) == 0 {
-            count -= 1000;
-            break;
-        }
-        for j in 0..6 {
-            if i == j {
-                continue;
-            }
+fn find(lists: &[&[u32]], start:u32, end:u32,found:&[u32]) {
+    if lists.len() == 0 && tail(*found.last().unwrap()) == end {
+        println!("found: {:?} {} {}", found, start, end);
+    }
 
-            if head(sample[i]) == tail(sample[j]) && tail(sample[i]) == head(sample[j]) {
-                count -= 100;
-            }
-            if head(sample[i]) == tail(sample[j]) {
-                count += 1;
-            }
-            if tail(sample[i]) == head(sample[j]) {
-                count += 1;
-            }
-            if head(sample[i]) == head(sample[j]) {
-                count -= 5;
-            }
-            if tail(sample[i]) == tail(sample[j]) {
-                count -= 5;
+    for &cur_list in lists {
+        for &n in cur_list {
+            if head(n) == start {
+                // remove cur_list
+                let  lists_copy = &lists[1..];
+                let mut found_new = found.to_vec();
+                found_new.push(n);
+                find(lists_copy, tail(n), end, &found_new);
+                //println!("{:?}", lists_copy);
+                //println!("{:?}", found_new);
             }
         }
     }
-    count
 }
-/*
-fn find(v: &Vec<(u32,u32,u32)>, start: u32, end: u32, poly_type: &[u32]) -> Vec<(u32,u32,u32)> {
-    for (head, tail, type) in v.iter() {
-        if poly_type.contains(type) && head == start {
-            let t = poly_type.clone().remove(&type);
-            let result = find(v, tail, end, &t);
-            if result.len() > 0 {
-                return result;
-            }
-        }
-    }
-    vec![]
-}
-*/
+
 fn gen_poly_numbers(start: u32, end: u32, f: fn(u32) -> u32) -> Vec<u32> {
     (1..)
         .map(f)
