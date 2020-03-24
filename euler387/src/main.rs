033cdceef36c5bf9033d2d3d38fd5f7016aac0f2
slofslb@qq.com <slofslb@qq.com>
2020-03-24 23:14:14 +0800

    ok

--- euler387/src/main.rs
+++ euler387/src/main.rs
@@ -1,51 +1,74 @@
-/*
-折腾的过程：
-1）直接按照题意，可以非常容易写出来，10000以内很快
-但数值大了之后，就会溢出
-2）改算法，提前生成素数表，但当数字大的时候，生成素数都非常花时间，还是算不出来，重复计算的地方太多
-3）梳理思路，把前面的一些数打印出来，分析规律，发现满足要求的数并不多
-前面的字头确定了之后，后面不会跑出那些范围
-4）可以用递归的方法，也不用提前生成素数，临时判断就行
-release模式，几秒算完，算法的力量
-*/
 fn main() {
-    for i in 1..=9 {
-        right_har(i);
+    /* 打印一些数，用于找规律
+    for i in 1..10_u64.pow(6) {
+        if is_right_truc_harshad(i) {
+            println!("{}", i);
+        }
     }
+    */
+
+    let mut sum = 0;
+    for i in 1..=9 {
+        sum += harshad_loop(i);
+    }
+    println!("{}", sum);
 }
 
-fn right_har(start: u64) {
-    if start >= 9_999_999_999_999 {
-        return;
+fn harshad_loop(head: u64) -> u64 {
+    if head >= 10_u64.pow(13) {
+        return 0;
     }
-    if right_harshad(start) {
-        if strong(start as usize) {
-            //println!("{}", start);
-            for j in (1..=9).step_by(2) {
-                let temp = start * 10 + j;
-                if primes::is_prime(temp) {
-                    println!("{}", temp);
-                    //sum += temp;
-                }
+    let mut sum = 0;
+    if is_right_truc_harshad(head) {
+        if is_strong_harshad(head) {
+            // 素数只能以1，3，5，7，9结尾
+            let harshad_primes = (1..=9)
+                .step_by(2)
+                .map(|i| head * 10 + i)
+                .filter(|&x| primes::is_prime(x))
+                .collect::<Vec<u64>>();
+            if !harshad_primes.is_empty() {
+                // println!("{:?}", harshad_primes);
             }
+            sum += harshad_primes.iter().sum::<u64>();
         }
         for i in 0..=9 {
-            right_har(start * 10 + i);
+            sum += harshad_loop(head * 10 + i);
         }
     }
+    sum
 }
 
-fn right_harshad(n: u64) -> bool {
-    let digits: Vec<u32> = n
-        .to_string()
+#[test]
+fn test_10000() {
+    let h: Vec<u64> = (1..100000000)
+        .filter(|x| is_right_truc_strong_harshad_prime(*x))
+        .collect();
+    println!("{:?}", h);
+    assert_eq!(h.iter().sum::<u64>(), 90619);
+}
+
+fn sum_of_digits(n: u64) -> u64 {
+    n.to_string()
         .chars()
         .map(|c| c.to_digit(10).unwrap())
-        .collect();
+        .sum::<u32>() as u64
+}
+
+// 哈沙德数: 能够被其各位数字和整除
+fn is_harshad(n: u64) -> bool {
+    n % sum_of_digits(n) == 0
+}
+
+// 可右截哈沙德数: 不断截去最后一位数字的结果始终是哈沙德数
+fn is_right_truc_harshad(n: u64) -> bool {
+    let s = n.to_string();
+    let digits = s.chars().map(|c| c.to_digit(10).unwrap() as u64);
     let mut num: u64 = 0;
     let mut sum: u64 = 0;
     for d in digits {
-        num = num * 10 + d as u64;
-        sum += d as u64;
+        num = num * 10 + d;
+        sum += d;
         if num % sum != 0 {
             return false;
         }
@@ -53,20 +76,14 @@ fn right_harshad(n: u64) -> bool {
     true
 }
 
-fn strong(n: usize) -> bool {
-    let digits: Vec<u32> = n
-        .to_string()
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect();
-    let mut num: u64 = 0;
-    let mut sum: u64 = 0;
-    for d in digits {
-        num = num * 10 + d as u64;
-        sum += d as u64;
-        if num % sum != 0 {
-            return false;
-        }
-    }
-    primes::is_prime((n as u64) / (sum as u64))
+use num_integer;
+fn is_strong_harshad(n: u64) -> bool {
+    let sum_digits = sum_of_digits(n);
+    let (q, r) = num_integer::div_rem(n, sum_digits);
+    r == 0 && primes::is_prime(q)
+}
+
+fn is_right_truc_strong_harshad_prime(n: u64) -> bool {
+    let m = n / 10;
+    m != 0 && primes::is_prime(n) && is_strong_harshad(m) && is_right_truc_harshad(m)
 }
