8149022f33a89cb8b2aa825824795910242bd467
Shen Longbin <slofslb@qq.com>
2020-03-27 16:41:44 +0800

    try

--- euler51/src/main.rs
+++ euler51/src/main.rs
@@ -1,23 +1,15 @@
 use itertools::Itertools;
 
 fn main() {
-    for n in 10..100 {
-        for pos in 0..=1 {
-            let prime_family = gen_prime_family(n, pos);
-            if prime_family.len() == 6 {
-                println!("seed: {} {:?}", n, prime_family);
-                break;
-            }
-        }
-    }
+    let family = find_prime_family_2digits(6);
+    assert_eq!(family, vec![13, 23, 43, 53, 73, 83]);
+
 
     let start = 100000;
     let end = 999999;
 
     for n in start..end {
-        if !primes::is_prime(n) {
-            continue;
-        }
+        // 替换2个以上的位置
         for position_replaced in 2..end.to_string().len() - 1 {
             let pos_combinations = (0..end.to_string().len()).combinations(position_replaced);
             for pos in pos_combinations {
@@ -47,32 +39,15 @@ fn main() {
 // seed: 120383 [1, 3, 5] [121313, 222323, 323333, 424343, 525353, 626363, 828383, 929393]
 // 121313
 
-static POWER: &[u64] = &[
-    1,
-    10,
-    100,
-    1_000,
-    10_000,
-    100_000,
-    1_000_000,
-    10_000_000,
-    100_000_000,
-    1_000_000_000,
-    10_000_000_000,
-];
-
 // 把整数n的某位换成新数字new_digit
 // pos为0表示个位，1表示十位，2表示百位
 fn replace(n: u64, pos: usize, new_digit: u64) -> u64 {
-    let d = n / 10_u64.pow(pos as u32) % 10;
-    n - d * 10_u64.pow(pos as u32) + new_digit * 10_u64.pow(pos as u32)
+    let p = 10_u64.pow(pos as u32);
+    let d = n / p % 10;    // 取出这个位置上的数字
+    n - d * p + new_digit * p
 }
 
-fn remove(n: u64, pos: usize) -> (u64, u64) {
-    let d = n / POWER[pos] % 10;
-    (n - d * POWER[pos], d)
-}
-
+// 一个数n换掉某位(pos)形成所有素数的集合
 fn gen_prime_family(n: u64, pos: usize) -> Vec<u64> {
     let mut prime_family = vec![];
     for digit in 0..=9 {
@@ -85,4 +60,18 @@ fn gen_prime_family(n: u64, pos: usize) -> Vec<u64> {
         }
     }
     prime_family
+}
+
+fn find_prime_family_2digits(num_primes: usize) -> Vec<u64> {
+    for n in 10..=99 {
+        // 先替换个位，再替换十位
+        for pos in 0..=1 {
+            let prime_family = gen_prime_family(n, pos);
+            if prime_family.len() == num_primes {
+                println!("seed: {} pos:{} {:?}", n, pos, prime_family);
+                return prime_family;
+            }
+        }
+    }
+    vec![]
 }
