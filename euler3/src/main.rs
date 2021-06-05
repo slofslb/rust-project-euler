fadf87f4064b66f03b0233d8f1549dcd936f92cf
slofslb@qq.com <slofslb@qq.com>
2021-06-05 16:55:31 +0800

    升级primes到0.3

--- euler3/src/main.rs
+++ euler3/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let big_num = 600_851_475_143;
-    let mut i = (big_num as f64).sqrt() as u64;
+    let n = 600_851_475_143;
+    let mut i = (n as f64).sqrt() as u64;
     while i > 0 {
-        if big_num % i == 0 && is_prime(i) {
+        if n % i == 0 && is_prime(i) {
             println!("{}", i);
             break;
         }
@@ -12,7 +12,10 @@ fn main() {
         println!("not found");
     }
     // 另一种方法，用primes函数库
-    println!("{:?}", primes::factors_uniq(600_851_475_143).last().unwrap());
+    println!(
+        "{:?}",
+        primes::factors_uniq(600_851_475_143).last().unwrap()
+    );
 
     //main_slow();
 }
@@ -28,9 +31,9 @@ fn is_prime(num: u64) -> bool {
 }
 
 fn main_slow() {
-    let big_num = 600_851_475_143;
-    for i in (1..=big_num / 2).rev() {
-        if big_num % i == 0 && is_prime(i) {
+    let n = 600_851_475_143;
+    for i in (1..=n / 2).rev() {
+        if n % i == 0 && is_prime(i) {
             println!("{}", i);
             break;
         }
